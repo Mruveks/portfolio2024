5c3b017e2d773852c7e35a616dc5a91f63ac7a23
Mruveks <Mruvekss@gmail.com>
2024-08-12 12:52:46 +0200

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from "react";
 import About from "./views/About";
 import CTA from "./views/CTA";
 import Projects from "./views/Projects";
 import Hero from "./views/Hero";
 import Tech from "./views/Tech";
 import Navigation from "./components/Navigation";
-import Footer from './views/Footer';
-
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import { Helmet } from "react-helmet";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import "./App.css";
 
 function App() {
   const sectionsRef = useRef([]);
-  const [activeSection, setActiveSection] = useState('hero');
+  const [activeSection, setActiveSection] = useState("hero");
   const scrollTimeoutRef = useRef(null);
+  const touchStartY = useRef(null);
 
   const handleScroll = (event) => {
     event.preventDefault();
@@ -22,13 +22,38 @@ function App() {
     if (scrollTimeoutRef.current) return;
 
     const direction = event.deltaY > 0 ? 1 : -1;
-    const currentIndex = sectionsRef.current.findIndex(section => section.id === activeSection);
+    const currentIndex = sectionsRef.current.findIndex((section) => section.id === activeSection);
     const nextIndex = currentIndex + direction;
 
     if (nextIndex >= 0 && nextIndex < sectionsRef.current.length) {
       setActiveSection(sectionsRef.current[nextIndex].id);
       sectionsRef.current[nextIndex].scrollIntoView({
-        behavior: 'smooth'
+        behavior: "smooth",
+      });
+
+      scrollTimeoutRef.current = setTimeout(() => {
+        scrollTimeoutRef.current = null;
+      }, 1000);
+    }
+  };
+
+  const handleTouchStart = (event) => {
+    touchStartY.current = event.touches[0].clientY;
+  };
+
+  const handleTouchEnd = (event) => {
+    const touchEndY = event.changedTouches[0].clientY;
+    const direction = touchEndY < touchStartY.current ? 1 : -1;
+
+    if (scrollTimeoutRef.current) return;
+
+    const currentIndex = sectionsRef.current.findIndex((section) => section.id === activeSection);
+    const nextIndex = currentIndex + direction;
+
+    if (nextIndex >= 0 && nextIndex < sectionsRef.current.length) {
+      setActiveSection(sectionsRef.current[nextIndex].id);
+      sectionsRef.current[nextIndex].scrollIntoView({
+        behavior: "smooth",
       });
 
       scrollTimeoutRef.current = setTimeout(() => {
@@ -38,11 +63,15 @@ function App() {
   };
 
   useEffect(() => {
-    const mainElement = document.querySelector('.smooth-scroll');
-    mainElement.addEventListener('wheel', handleScroll);
+    const mainElement = document.querySelector(".smooth-scroll");
+    mainElement.addEventListener("wheel", handleScroll);
+    mainElement.addEventListener("touchstart", handleTouchStart);
+    mainElement.addEventListener("touchend", handleTouchEnd);
 
     return () => {
-      mainElement.removeEventListener('wheel', handleScroll);
+      mainElement.removeEventListener("wheel", handleScroll);
+      mainElement.removeEventListener("touchstart", handleTouchStart);
+      mainElement.removeEventListener("touchend", handleTouchEnd);
     };
   }, [activeSection]);
 
@@ -71,14 +100,29 @@ function App() {
 
   return (
     <div className="app">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Kuba Mrowiec</title>
+        <meta name="description" content="Personal Portfolio" />
+        <link rel="icon" href="/path/to/your/favicon.ico" />
+      </Helmet>
       <Navigation activeSection={activeSection} />
       <main className="smooth-scroll">
-        <section id="hero" ref={el => sectionsRef.current[0] = el}><Hero /></section>
-        <section id="about" ref={el => sectionsRef.current[1] = el}><About /></section>
-        <section id="projects" ref={el => sectionsRef.current[2] = el}><Projects /></section>
-        <section id="tech" ref={el => sectionsRef.current[3] = el}><Tech /></section>
-        <span id="contact" ref={el => sectionsRef.current[4] = el}><CTA /></span>
-        <span><Footer /></span>
+        <section id="hero" ref={(el) => (sectionsRef.current[0] = el)}>
+          <Hero />
+        </section>
+        <section id="about" ref={(el) => (sectionsRef.current[1] = el)}>
+          <About />
+        </section>
+        <section id="projects" ref={(el) => (sectionsRef.current[2] = el)}>
+          <Projects />
+        </section>
+        <section id="tech" ref={(el) => (sectionsRef.current[3] = el)}>
+          <Tech />
+        </section>
+        <section id="contact" ref={(el) => (sectionsRef.current[4] = el)}>
+          <CTA />
+        </section>
       </main>
     </div>
   );
--- src/views/CTA.jsx
+++ src/views/CTA.jsx
@@ -1,41 +1,45 @@
 import React from "react";
+import Footer from "./Footer";
 
 const CTA = () => {
   return (
-    <div className=" jjju flex flex-col justify-center py-16  px-64 ">
-      <article className="bg-olive-green px-16 py-32 text-white grid grid-cols-2">
-        <span>
-          <h1 className="text-2xl relative flex flex-col w-full justify-center">
-            Looking to collaborate or learn more about my work?
-          </h1>
-          <h2 className="text-xl">Get in touch with me for more information.</h2>
-        </span>
-        <span className="text-xl flex justify-around">
-          <button>
-            <a
-              href="https://github.com/Mruveks"
-              target="__blank"
-              className="border px-4 py-2 rounded-xl hover:bg-moss-green"
-            >
-              Github
-            </a>
-          </button>{" "}
-          <button>
-            <a href="mailto:mrowiec.kuba.mk@gmail.com" className="border px-4 py-2 rounded-xl hover:bg-moss-green">
-              E-mail
-            </a>
-          </button>
-          <button>
-            <a
-              href="https://www.linkedin.com/in/kuba-mrowiec-0630b2244/"
-              target="__blank"
-              className="border px-4 py-2 rounded-xl hover:bg-moss-green"
-            >
-              Linkedin
-            </a>
-          </button>
-        </span>
+    <div className="flex flex-col justify-between h-full">
+      <article className="px-64 flex flex-col justify-center h-full">
+        <section className="p-16 bg-olive-green items-center h-64 text-white grid grid-cols-2">
+          <span>
+            <h1 className="text-2xl relative flex flex-col w-full justify-center">
+              Looking to collaborate or learn more about my work?
+            </h1>
+            <h2 className="text-xl">Get in touch with me for more information.</h2>
+          </span>
+          <span className="text-xl flex justify-around">
+            <button>
+              <a
+                href="https://github.com/Mruveks"
+                target="__blank"
+                className="border px-4 py-2 rounded-xl hover:bg-moss-green"
+              >
+                Github
+              </a>
+            </button>{" "}
+            <button>
+              <a href="mailto:mrowiec.kuba.mk@gmail.com" className="border px-4 py-2 rounded-xl hover:bg-moss-green">
+                E-mail
+              </a>
+            </button>
+            <button>
+              <a
+                href="https://www.linkedin.com/in/kuba-mrowiec-0630b2244/"
+                target="__blank"
+                className="border px-4 py-2 rounded-xl hover:bg-moss-green"
+              >
+                Linkedin
+              </a>
+            </button>
+          </span>
+        </section>
       </article>
+      <Footer />
     </div>
   );
 };
